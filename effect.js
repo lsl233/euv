8d52d2da8672657d73a21b6ef2b7ee820c8e720b
lsl233 <ahands233@gmail.com>
2024-03-28 21:38:21 +0800

    4.1 响应式数据基本实现

--- effect.js
+++ effect.js
@@ -1,6 +1,29 @@
-const obj = {
+
+
+const bucket = new Set()
+const data = {
     text: 'hello euv'
 }
+
+const obj = new Proxy(data, {
+    get(target, key) {
+        bucket.add(effect)
+        return target[key]
+    },
+
+    set(target, key, newVal) {
+        target[key] = newVal
+        bucket.forEach(fn => fn())
+        return true
+    }
+
+})
 function effect() {
     document.body.innerText = obj.text
 }
+
+effect()
+
+setTimeout(() => {
+    obj.text = 'euv hello'
+}, 3000)
