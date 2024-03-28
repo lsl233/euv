ebb45246e9cffe23b2b32ca05ea03486b2184a9d
lsl233 <ahands233@gmail.com>
2024-03-28 21:52:55 +0800

    4.3 设计一个完善的响应系统

--- effect.js
+++ effect.js
@@ -1,28 +1,64 @@
 
+let activeEffect
 
-const bucket = new Set()
+// target -> key -> effectFn
+const bucket = new WeakMap()
+
 const data = {
     text: 'hello euv'
 }
 
+/**
+ * 依赖收集
+ */
+function track(target, key) {
+    if (activeEffect) {
+        let depsMap = bucket.get(target)
+        if (!depsMap) {
+            depsMap = new Map()
+            bucket.set(target, depsMap)
+        }
+        let deps = depsMap.get(key)
+        if (!deps) {
+            deps = new Set()
+            depsMap.set(key, deps)
+        }
+        deps.add(activeEffect)
+    }
+}
+
+/**
+ * 触发更新
+ */
+function trigger(target, key) {
+    const depsMap = bucket.get(target)
+    if (!depsMap) return
+    const deps = depsMap.get(key)
+    if (!deps) return
+    deps.forEach(fn => fn())
+}
+
 const obj = new Proxy(data, {
     get(target, key) {
-        bucket.add(effect)
+        track(target, key)
         return target[key]
     },
 
     set(target, key, newVal) {
         target[key] = newVal
-        bucket.forEach(fn => fn())
+        trigger(target, key)
         return true
     }
 
 })
-function effect() {
-    document.body.innerText = obj.text
+function effect(fn) {
+    activeEffect = fn
+    fn()
 }
 
-effect()
+effect(() => {
+    document.body.innerText = obj.text
+})
 
 setTimeout(() => {
     obj.text = 'euv hello'
